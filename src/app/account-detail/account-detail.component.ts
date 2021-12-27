8555a2b161554d5531d69f4d938d3b81ae21a17c
Michael Marino <michael.marino@TIG365.onmicrosoft.com>
2021-12-27 10:41:05 -0500

    added routing with params

---
+++ src/app/account-detail/account-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+@Component({
+  selector: 'app-account-detail',
+  templateUrl: './account-detail.component.html',
+  styleUrls: ['./account-detail.component.css']
+})
+export class AccountDetailComponent implements OnInit {
+  id: string | null = '';
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = params.get('id');
+    });
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { AccountDetailComponent } from './account-detail/account-detail.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'account', component: AccountComponent},
+  {path: 'account/:id', component: AccountDetailComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AccountComponent,
+    AccountDetailComponent
   ],
   imports: [
     BrowserModule,
